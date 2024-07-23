e8482021db354624373e3138274d74e9c4375a95
AshKuraine <lpc9720@hotmail.com>
2024-07-22 22:09:29 -0500

    fix square

--- DibujarCuadrado.java
+++ DibujarCuadrado.java
@@ -4,14 +4,14 @@ public class DibujarCuadrado {
         Scanner entrada = new Scanner(System.in);
         System.out.print("Ingrese el lado del largo del cuadrado: ");
         int entero = entrada.nextInt();
-        for (int i = 0; i <= entero; i++) {
-            if (i == 0 || i == entero) {
+        for (int i = 0; i < entero; i++) {
+            if (i == 0 || i == entero-1) {
                 for (int j = 0; j < entero; j++) {
                     System.out.print("*");
                 }
                 System.out.println();
             } else {
-                for (int j = 0; j <= entero-2; j++) {
+                for (int j = 0; j < entero; j++) {
                     if (j == 0 || j == entero-1) {
                         System.out.print("*");
                     } else {
